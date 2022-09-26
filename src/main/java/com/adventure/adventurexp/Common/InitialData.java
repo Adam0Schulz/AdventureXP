889d2267cd639a49afa53b8aba4dd0893e513de9
Mubíbic <90600712+mubibic@users.noreply.github.com>
2022-09-26 12:56:18 +0200

    updated descriptions

--- src/main/java/com/adventure/adventurexp/Common/InitialData.java
+++ src/main/java/com/adventure/adventurexp/Common/InitialData.java
@@ -17,10 +17,10 @@ public class InitialData implements CommandLineRunner {
 
     @Override
     public void run(String... args) throws Exception {
-        Activity goKart = new Activity("Gokart","It's a funny activity");
-        Activity sumoWrestling = new Activity("Sumo Wrestling", "The participants will wear an inflatable sumo suit, and wrestle each other. The purpose is to push the opponent outside of the wrestling ring. ");
-        Activity miniGolf = new Activity("Minigolf", "Golf, but mini");
-        Activity paintBall = new Activity("Paintball","Two teams will go against each other. THe purpose is to shot the opposing team with paintballs until only members from one team is left");
+        Activity goKart = new Activity("Gokart","In go-karting, the participants drive around a course racing the be the first one across the finish line after 20 laps. Points are given based on placement. The participant with the fastest lap will receive 5 bonus points. Age limit: 16+.");
+        Activity sumoWrestling = new Activity("Sumo Wrestling", "The participants will wear an inflatable sumo suit and wrestle each other. The purpose is to push the opponent outside of the wrestling ring. Age limit: 11+. ");
+        Activity miniGolf = new Activity("Minigolf", "Golf, but mini. It's played on courses consisting of 24 holes. Players aim is to have the least amount of attempts at the end of the game. Players have a maximum of 7 tries per hole. Age limit: 5+. ");
+        Activity paintBall = new Activity("Paintball","Two teams will go against each other. THe purpose is to shoot the opposing team with paint balls until only members from one team is left. Age limit: 18+.");
         activityRepository.save(goKart);
         activityRepository.save(sumoWrestling);
         activityRepository.save(miniGolf);

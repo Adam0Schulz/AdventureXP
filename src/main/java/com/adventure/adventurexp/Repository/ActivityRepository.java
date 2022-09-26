009d9af414f8ea0f3fd9a6f935e2884ed105d7e3
ventsi002 <viliev2002@gmail.com>
2022-09-26 12:49:00 +0200

    activity services done

--- src/main/java/com/adventure/adventurexp/Repository/ActivityRepository.java
+++ src/main/java/com/adventure/adventurexp/Repository/ActivityRepository.java
@@ -4,4 +4,6 @@ import com.adventure.adventurexp.Entity.Activity;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface ActivityRepository extends JpaRepository<Activity, Long> {
+
+    Long countById(Long id);
 }
--- src/main/java/com/adventure/adventurexp/Service/ActivityService.java
+++ src/main/java/com/adventure/adventurexp/Service/ActivityService.java
@@ -1,8 +1,11 @@
 package com.adventure.adventurexp.Service;
 
+import com.adventure.adventurexp.Entity.Activity;
 import com.adventure.adventurexp.Repository.ActivityRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+
+import java.util.List;
 
 @Service
 public class ActivityService {
@@ -15,4 +18,22 @@ public class ActivityService {
         this.activityRepo = activityRepo;
     }
 
+    public List<Activity> getAllActivities ()
+    {
+        List<Activity> activityList = activityRepo.findAll();
+        return activityList;
+    }
+
+    public Activity getActivityById(Long id) {
+        return activityRepo.findById(id).get();
+    }
+
+    public Activity createActivity(Activity activity) {
+        return activityRepo.save(activity);
+    }
+
+    public void deleteActivity(Long id) {
+        Long count = activityRepo.countById(id);
+        activityRepo.deleteById(id);
+    }
 }

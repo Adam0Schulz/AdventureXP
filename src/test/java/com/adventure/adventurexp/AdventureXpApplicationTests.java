159d86017bfcc6ab4a6751a86a909b24964b13ed
Dániel Szabó <kingalistairlol@gmail.com>
2022-09-28 09:29:21 +0200

    Code Cleanup

--- src/test/java/com/adventure/adventurexp/AdventureXpApplicationTests.java
+++ src/test/java/com/adventure/adventurexp/AdventureXpApplicationTests.java
@@ -15,8 +15,6 @@ import java.util.Optional;
 @DataJpaTest
 class AdventureXpApplicationTests {
 
-
-
     ActivityRepository activityRepository;
 
     @Autowired
@@ -24,16 +22,12 @@ class AdventureXpApplicationTests {
         this.activityRepository = activityRepository;
     }
 
-
-
-
     @Test
     void addActivity() {
         Activity goKart = new Activity("Gokart","In go-karting, the participants drive around a course racing the be the first one across the finish line after 20 laps. Points are given based on placement. The participant with the fastest lap will receive 5 bonus points. Age limit: 16+.");
         Activity sumoWrestling = new Activity("Sumo Wrestling", "The participants will wear an inflatable sumo suit and wrestle each other. The purpose is to push the opponent outside of the wrestling ring. Age limit: 11+. ");
         activityRepository.save(goKart);
         activityRepository.save(sumoWrestling);
-
     }
 
     @Test
@@ -42,8 +36,6 @@ class AdventureXpApplicationTests {
         activityRepository.save(sumoWrestling);
         activityRepository.delete(sumoWrestling);
     }
-
-
 
     @Test
    void findByIdActivity(){
@@ -56,13 +48,6 @@ class AdventureXpApplicationTests {
     void deleteByIdActivity(){
         Activity sumoWrestling = new Activity("Sumo Wrestling", "The participants will wear an inflatable sumo suit and wrestle each other. The purpose is to push the opponent outside of the wrestling ring. Age limit: 11+. ");
         activityRepository.save(sumoWrestling);
-        activityRepository.deleteById(3L);
+        activityRepository.deleteById(3l);
     }
-
-
-
-
-
-
-
 }

deeb50a65934c643fab7f8c9ab40977c65a4b038
Dániel Szabó <kingalistairlol@gmail.com>
2022-09-30 09:27:47 +0200

    Lombok re-imported in Activity class.

--- src/main/java/com/adventure/adventurexp/Entity/Activity.java
+++ src/main/java/com/adventure/adventurexp/Entity/Activity.java
@@ -1,7 +1,9 @@
 package com.adventure.adventurexp.Entity;
 
-import jdk.jshell.Snippet;
-import lombok.*;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
 
 import javax.persistence.*;
 

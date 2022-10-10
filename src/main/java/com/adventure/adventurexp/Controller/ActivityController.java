46b2195d82b8491f5f3b3f78a2f5efad8dbd648e
Adam0Schulz <adam@schulz.sk>
2022-10-10 09:01:30 +0200

    added some things to work with bookings page

--- src/main/java/com/adventure/adventurexp/Controller/ActivityController.java
+++ src/main/java/com/adventure/adventurexp/Controller/ActivityController.java
@@ -3,11 +3,14 @@ package com.adventure.adventurexp.Controller;
 import com.adventure.adventurexp.Entity.Activity;
 import com.adventure.adventurexp.Entity.Booking;
 import com.adventure.adventurexp.Service.ActivityService;
+import com.adventure.adventurexp.Service.BookingService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
+import java.time.LocalDate;
+import java.util.ArrayList;
 import java.util.List;
 
 @RestController
@@ -17,9 +20,12 @@ public class ActivityController {
     //Dependencies
     private final ActivityService activityService;
 
+    private final BookingService bookingService;
+
     @Autowired
-    public ActivityController(ActivityService activityService) {
+    public ActivityController(ActivityService activityService, BookingService bookingService) {
         this.activityService = activityService;
+        this.bookingService = bookingService;
     }
 
     //Return all activities
@@ -28,16 +34,34 @@ public class ActivityController {
         return activityService.getAllActivities();
     }
 
-    //Create a method that returns a single activity
+    //Return a single activity
     @GetMapping("/activities/{id}")
     public ResponseEntity<Activity> getActivityById(@PathVariable("id") Long id){
         Activity activity = activityService.getActivityById(id);
         return new ResponseEntity<>(activity, HttpStatus.OK);
     }
 
+    //Return all bookings connected to the activity that match provided date
     @GetMapping("/activities/{id}/bookings")
-    public List<Booking> getBookingsByActivity(@PathVariable("id") Long id) {
-        return activityService.getActivityById(id).getBookings().stream().toList();
+    public List<Booking> getBookingsByActivity(@PathVariable("id") Long id, @RequestParam(name = "date") String date) {
+        List<Booking> bookingList = activityService.getActivityById(id).getBookings().stream().toList();
+        List<Booking> searchedBookings = new ArrayList<>();
+
+        String[] result = date.split("-");
+        int year = Integer.parseInt(result[0]);
+        int month = Integer.parseInt(result[1]);
+        int day = Integer.parseInt(result[2]);
+
+        LocalDate localDate = LocalDate.of(year, month, day);
+
+        for (Booking booking : bookingList
+        ) {
+            if (booking.getDate().isEqual(localDate)) {
+                searchedBookings.add(booking);
+            }
+        }
+        return searchedBookings;
+        //return activityService.getActivityById(id).getBookings().stream().toList();
     }
 
     //Create an activity
